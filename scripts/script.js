8ad0c07cd073bdd5ea0e9b7ad390edbffd9145fe
Patrick M <patrickymacdonald@gmail.com>
2023-11-07 14:08:35 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,4 @@
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
+/*
 const threshold = 0.9 * window.innerHeight;
 console.log(window.innerHeight);
 console.log(threshold);
@@ -31,3 +21,4 @@ function updateOpacity() {
 
 window.addEventListener('scroll', updateOpacity);
 window.addEventListener('load', updateOpacity);
+*/
